766ab1f1d64172eb87a9c680a7c9129cb046efe3
Quentin Godefroi <qgodefroi@gmail.com>
2018-02-19 14:36:07 +0100

    Fix handling of coroutine return value
    Store it in the promise directly, use a variant to put it in
    the same place as options as only one of them can exist at
    any given time
    
    Improve the API of nondeterminate to easily access the result
    or the options provided

--- coro.hpp
+++ coro.hpp
@@ -2,7 +2,7 @@
 
 #include <any>
 #include <experimental/coroutine>
-#include <optional>
+#include <variant>
 #include <vector>
 
 struct choice_base {
@@ -21,12 +21,12 @@ struct choice : choice_base {
     template <class U>
     void await_suspend(
         std::experimental::coroutine_handle<U> h) noexcept {
-        h.promise().options.emplace(
+        h.promise().options_or_result =
             [this]() -> cppcoro::generator<std::any> {
-                for (auto ch : *choices) {
-                    co_yield{ch};
-                }
-            }());
+            for (auto ch : *choices) {
+                co_yield{ch};
+            }
+        }();
         h.promise().awaiting = this;
     }
 
@@ -62,11 +62,14 @@ struct nondeterministic {
 
         void unhandled_exception() noexcept { std::terminate(); }
 
-        void return_value(T&& v) noexcept { return_value(v); }
-        void return_value(T& v) noexcept { value = &v; }
+        template <class ReturnedT>
+        void return_value(ReturnedT&& v) noexcept {
+            options_or_result.template emplace<T>(
+                std::forward<ReturnedT>(v));
+        }
 
-        T* value = nullptr;
-        std::optional<cppcoro::generator<std::any>> options;
+        std::variant<std::monostate, T, cppcoro::generator<std::any>>
+            options_or_result;
         choice_base* awaiting = nullptr;
     };
 
@@ -96,16 +99,27 @@ struct nondeterministic {
     handle_type coro;
 
     // coroutine methods
-    auto operator()() {
-        coro.resume();
-        return std::move(coro.promise().options.value());
-    }
+    void operator()() { coro.resume(); }
 
-    auto send(std::any const& pick) {
+    void send(std::any const& pick) {
         // send pick to awaiter
         coro.promise().awaiting->pick = pick;
         // resume
-        return (*this)();
+        (*this)();
+    }
+
+    T& result() & {
+        return std::get<T>(coro.promise().options_or_result);
+    }
+
+    T&& result() && {
+        return std::move(
+            std::get<T>(coro.promise().options_or_result));
+    }
+
+    cppcoro::generator<std::any>& options() {
+        return std::get<cppcoro::generator<std::any>>(
+            coro.promise().options_or_result);
     }
 };
 
@@ -123,21 +137,21 @@ struct list {
         auto g = genfunc();
 
         // call it until it stops on a choice
-        auto possible_choices = g();
+        g();
         // repeat all previous choices
         for (auto id : chosen_picks) {
-            possible_choices = g.send(*id);
+            g.send(*id);
         }
 
         // we've reached the end, yield the final result
         if (g.coro.done()) {
-            co_yield std::move(*g.coro.promise().value);
+            co_yield std::move(g.result());
         }
 
         // if the coroutine is stuck on a choice, restart with each
         // possibility
         else {
-            for (auto new_pick : possible_choices) {
+            for (auto new_pick : g.options()) {
                 auto new_picks = chosen_picks;
                 new_picks.emplace_back(&new_pick);
                 co_yield (*this)(new_picks);
